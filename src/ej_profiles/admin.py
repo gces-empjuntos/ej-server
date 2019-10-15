f8865e8219cb74a40d02191c8a764214e22a8ce5
Hiroshi <lucas.hh@hotmail.com>
2019-10-15 14:58:39 -0300

    add field in admin

--- src/ej_profiles/admin.py
+++ src/ej_profiles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from constance import config
 
 from . import models
 
@@ -8,13 +9,35 @@ descr = lambda msg: lambda f: setattr(f, "short_description", msg) or f
 
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    config.EJ_PROFILE_PHOTO
+    config.EJ_PROFILE_RACE
+    config.EJ_PROFILE_GENDER
+    config.EJ_PROFILE_BIRTH_DATE
+    config.EJ_PROFILE_OCCUPATION
+
+    config.EJ_PROFILE_CITY
+    config.EJ_PROFILE_STATE
+    config.EJ_PROFILE_COUNTRY
+
+    config.EJ_PROFILE_POLITICAL_ACTIVITY
+    config.EJ_PROFILE_BIOGRAPHY
+
     fieldsets = (
         (
             _("Basic information"),
-            {"fields": ["profile_photo", "race", "gender", "birth_date", "occupation"]},
+            {"fields": ["profile_photo", 
+                        "race", 
+                        "gender", 
+                        "birth_date" , 
+                        "occupation" ]},
         ),
-        (_("Address"), {"fields": ["city", "state", "country"]}),
-        (_("Advanced"), {"fields": ["political_activity", "biography"]}),
+        (_("Address"), {"fields": ["city", 
+                                   "state", 
+                                   "country"]}),
+
+        (_("Advanced"), {"fields": ["political_activity", 
+                                    "biography"]}),
     )
     list_display = ("name", "email", "is_superuser")
     search_fields = ["user__name", "user__email"]
+
