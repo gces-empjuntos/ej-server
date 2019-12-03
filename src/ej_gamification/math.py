0155f86dd9dbbf59262045540e481d12b0dd72f1
Carlos Aragon <carloseraragon@gmail.com>
2019-12-03 12:28:35 -0300

    fix codeclimate issue

--- src/ej_gamification/math.py
+++ src/ej_gamification/math.py
@@ -15,7 +15,9 @@ def calculate_centroids(data, labels):
    return centroid_array
 
 
-def calculate_distances(distances, label_set, labels, centroid_index, data_index):
+def calculate_distances(distances, labels_list, centroid_index, data_index):
+    label_set = labels_list[0]
+    labels = labels_list[1]
     if label_set[centroid_index] == labels[data_index]:
         distances[data_index, centroid_index] = float("inf")
     else:
@@ -35,9 +37,11 @@ def compute_opinion_bridge_index(df, labels):
 
     distances = np.empty([n_samples, k])
 
+    labels_list = [label_set,labels]
+
     for i, sample in enumerate(data):
         for j, centroid in enumerate(centroids):
-            calculate_distances(distances, label_set, labels, j, i)
+            calculate_distances(distances, labels_list, j, i)
     return distances.min(axis=1)
 
 
